22414edbeb6b3296bfeb8ed887235c32668daec0
aklein <aklein@student.hive.fi>
2024-05-07 21:38:34 +0300

    random lexxer test stuff

--- include/minishell.h
+++ include/minishell.h
@@ -5,5 +5,29 @@
 # include <readline/history.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <libft.h>
+
+# define TOKEN_BUFFER 64
+# define TOKENS "|<>\"\'$"
+
+typedef struct s_token t_token;
+
+
+typedef enum e_tok
+{
+	PIPE = '|',
+	LESS = '<',
+	GREAT = '>',
+	S_QUOTE = '\'',
+	D_QUOTE = '\"',
+	DOLLAR = '$',
+	WORD
+}	t_tok;
+
+struct s_token
+{
+	char	*str;
+	t_tok	type;
+};
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -1,13 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/07 20:58:22 by aklein            #+#    #+#             */
+/*   Updated: 2024/05/07 21:35:48 by aklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minishell.h>
+
+int	ft_is_whitespace(char c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
+
+int	word_length(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str && !ft_strchr(TOKENS, *str) && !ft_is_whitespace(*str))
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+char	*ft_skip_whitespace(char *str)
+{
+	while (ft_is_whitespace(*str))
+		str++;
+	return (str);
+}
+
+t_list	*lexer(char *str)
+{
+	t_list	*tokens;
+	t_list	*new;
+	t_token	*current_tok;
+	int		word_len;
+
+	str = ft_skip_whitespace(str);
+	tokens = NULL;
+	while (*str)
+	{
+		str = ft_skip_whitespace(str);
+		current_tok = ft_calloc(1, sizeof(t_token));
+		if (ft_strchr(TOKENS, *str))
+		{
+			current_tok->type = *str;
+			current_tok->str = NULL;
+			new = ft_lstnew((void *)current_tok);
+			ft_lstadd_back(&tokens, new);
+			str++;
+		}
+		else
+		{
+			word_len = word_length(str);
+			current_tok->type = WORD;
+			current_tok->str = ft_substr(str, 0, word_len);
+			new = ft_lstnew((void *)current_tok);
+			ft_lstadd_back(&tokens, new);
+			str += word_len;
+		}
+	}
+	return (tokens);
+}
 
 int main(void)
 {
 	char *line;
+	t_list *tokens;
+	t_token *tok;
 
 	while (42)
 	{
 		line = readline("Minishell:>");
-		printf("%s\n", line);
+		tokens = lexer(line);
+		while (tokens)
+		{
+			tok = (t_token *)(tokens->content);
+			printf("<type: '%c' str: <%s>\n", tok->type, tok->str);
+			tokens = tokens->next;
+		}
+		// printf("%s\n", line);
 		free(line);
 	}
 	return (0);

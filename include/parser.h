8594c9e6f9eafc1dff70f680d698a8951e3b1925
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-05-09 10:27:27 +0000

    Refactor parser.h for improved code organization and readability

--- include/parser.h
+++ include/parser.h
@@ -6,18 +6,24 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 10:11:14 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/09 10:16:01 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/09 10:27:23 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-// a simple command and arguments
+//single command
 typedef struct s_simple_cmd
 {
-	char	**args; 		// array of arguments
-	int		num_of_args;	// number of arguments
-	
+	char	*value;	// value of the command
 }				t_simple_cmd;
+
+// collection of commands
+typedef struct s_commands
+{
+	t_simple_cmd	**args; 		// array of arguments
+	int				num_of_args;	// number of arguments
+	
+}				t_commands;
 #endif

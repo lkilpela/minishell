081407205ae789d507ee9e02da2626205ad063ab
lumikil <kilpelainenlumi@gmail.com>
2024-05-10 11:06:35 +0300

    Refactor tokenizer.c and add init.c

--- src/init.c
+++ src/init.c
@@ -1,14 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/09 21:09:48 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/10 11:03:29 by lkilpela         ###   ########.fr       */
+/*   Created: 2024/05/10 11:04:00 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/05/10 11:06:15 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+void init_minishell(int argc, char **argv, char **envp, t_minishell *m)
+{
+	m->argc = argc;
+	m->argv = argv;
+	m->envp = envp;
+}

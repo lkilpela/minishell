b8737ad661ca772dbd39e94d7a8a4343aa83958d
lumikil <kilpelainenlumi@gmail.com>
2024-05-10 15:55:04 +0300

    add free.c

---
+++ src/free.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/10 15:51:27 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/05/10 15:54:53 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+#include <tokenizer.h>
+
+void	free_arrays(char **arrays)
+{
+	int	i;
+
+	i = 0;
+	while (arrays[i])
+	{
+		free(arrays[i]);
+		i++;
+	}
+	free(arrays);
+}

7e3d995f2cd018292d9c08697a49f15fda38056c
lumikil <kilpelainenlumi@gmail.com>
2024-05-10 13:04:28 +0300

    Refactor list.c

--- src/list.c
+++ src/list.c
@@ -6,13 +6,13 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 15:18:16 by aklein            #+#    #+#             */
-/*   Updated: 2024/05/10 12:34:42 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/10 13:03:41 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <tokenizer.h>
 
-t_token_list	*new_node(t_token_list *head, t_token token)
+t_token_list	*new_node(t_token_list *lst, t_token token)
 {
 	t_token_list *node;
 
@@ -23,14 +23,29 @@ t_token_list	*new_node(t_token_list *head, t_token token)
 	node->next = NULL;
 	return (node);
 }
-
-t_token_list	*tok_lstlast(t_token_list *lst)
+t_token_list	*last_node(t_token_list *lst)
 {
 	if (!lst)
 		return (NULL);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
+}
+
+t_token_list	*add_node(t_token_list **lst, t_token token)
+{
+	t_token_list	*last;
+
+	if (!lst)
+		return (NULL);
+	if (!*lst)
+		new_node(lst, token);
+	else
+	{
+		last = last_node(*lst);
+		new_node(&(last->next), token);
+	}
+	return (*lst);
 }
 
 void	tok_lstadd_back(t_token_list **lst, char *str, t_token type)

b40c17fb8804313110adbe16c1429b251e902e78
aklein <aklein@student.hive.fi>
2024-05-11 00:48:20 +0300

    somewhat working parser, still need to fix num_of_commands heredoc errors and unknowns stuff

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 10:11:14 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/10 15:55:41 by aklein           ###   ########.fr       */
+/*   Updated: 2024/05/10 23:06:30 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,20 @@
 # define PARSER_H
 
 //single command
+typedef struct s_redir
+{
+	char	*file;
+	int		append;
+	int		fd;
+}	t_redir;
+
+
 typedef struct s_simple_cmd
 {
 	char	*command;	// value of the command
 	int		num_of_args;	// number of arguments
-	char	*in_file;
-	char	*out_file;
+	t_redir	in_file;
+	t_redir	out_file;
 	char	**args;
 }			t_simple_cmd;
 
@@ -27,5 +35,6 @@ typedef struct s_simple_cmd
 typedef struct s_commands
 {
 	t_simple_cmd	**commands;		// array of simple cmds
+	int				num_of_cmds;
 }				t_commands;
 #endif
--- src/parsing.c
+++ src/parsing.c
@@ -8,27 +8,45 @@ t_token_list	*init_test()
 	t_token_list	*ptr;
 	test = malloc(sizeof(t_token_list));
 	ptr = test;
+
 	test->token.value = "ls";
 	test->token.type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+
+	test->token.value = ">";
+	test->token.type = GREAT;
+	test->next = malloc(sizeof(t_token_list));
+	test = test->next;
+
+	test->token.value = "myout";
+	test->token.type = WORD;
+	test->next = malloc(sizeof(t_token_list));
+	test = test->next;
+
 	test->token.value = "-l";
 	test->token.type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+
 	test->token.value = "|";
 	test->token.type = PIPE;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+
 	test->token.value = "cat";
 	test->token.type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+
 	test->token.value = "-e";
 	test->token.type = WORD;
 	test->next = NULL;
+
 	return (ptr);
 }
+
+
 
 int	count_args(t_token_list *tokens)
 {
@@ -37,6 +55,15 @@ int	count_args(t_token_list *tokens)
 	count = 0;
 	while (tokens && tokens->token.type != PIPE)
 	{
+		if (tokens->token.type >= LESS && tokens->token.type <=D_GREAT)
+		{
+			tokens = tokens->next;
+			if (tokens->token.type == WORD)
+			{
+				tokens = tokens->next;
+				continue ;
+			}
+		}
 		if (tokens->token.type == WORD)
 			count++;
 		tokens = tokens->next;
@@ -44,38 +71,80 @@ int	count_args(t_token_list *tokens)
 	return (count);
 }
 
+void	heredoc(void)
+{
+	return ;
+}
+
+t_token_list	*get_redir(t_simple_cmd *simple, t_token_list *tokens)
+{
+	if (tokens->token.type == D_LESS)
+		heredoc();
+	if (tokens->token.type == LESS)
+	{
+		tokens = tokens->next;
+		if (tokens->token.type == WORD)
+		{
+			simple->in_file.file = tokens->token.value;
+			return (tokens->next);
+		}
+	}
+	if (tokens->token.type == GREAT || tokens->token.type == D_GREAT)
+	{
+		if (tokens->token.type == D_GREAT)
+			simple->out_file.append = 1;
+		tokens = tokens->next;
+		if (tokens->token.type == WORD)
+		{
+			simple->out_file.file = tokens->token.value;
+			return (tokens->next);
+		}
+	}
+	return (tokens);
+}
+
+t_simple_cmd	*simple_cmd(t_token_list **tokens)
+{
+	t_simple_cmd	*simple_cmd;
+	int				i;
+
+	simple_cmd = ft_calloc(1, sizeof(t_simple_cmd));
+	while ((*tokens) && (*tokens)->token.type != PIPE)
+	{
+		if ((*tokens)->token.type >= LESS && (*tokens)->token.type <= D_GREAT)
+		{
+			(*tokens) = get_redir(simple_cmd, *tokens);
+			continue ;
+		}
+		if (simple_cmd->command == NULL && (*tokens)->token.type == WORD)
+		{
+			simple_cmd->command = (*tokens)->token.value;
+			(*tokens) = (*tokens)->next;
+			simple_cmd->num_of_args = count_args(*tokens);
+			simple_cmd->args = ft_calloc(simple_cmd->num_of_args, sizeof(char *));
+			i = 0;
+			continue ;
+		}
+		if (simple_cmd->command != NULL && (*tokens)->token.type == WORD)
+			simple_cmd->args[i++] = (*tokens)->token.value;
+		(*tokens) = (*tokens)->next;
+	}
+	return (simple_cmd);
+}
+
 t_commands	*parser(t_token_list *tokens)
 {
 	t_commands		*commands;
-	t_simple_cmd	*simple_cmd;
-	int	i;
 	int	cmd_i;
 
 	commands = ft_calloc(1, sizeof(t_commands));
-	commands->commands = ft_calloc(10, sizeof(t_simple_cmd *));
+	commands->commands = ft_calloc(10, sizeof(t_simple_cmd *)); //count commands and change this
 	cmd_i = 0;
 	while (tokens)
 	{
-		simple_cmd = ft_calloc(1, sizeof(t_simple_cmd));
-		if (tokens->token.type == PIPE)
-		{
+		commands->commands[cmd_i++] = simple_cmd(&tokens);
+		if (tokens)
 			tokens = tokens->next;
-			continue ;
-		}
-		else
-			simple_cmd->command = tokens->token.value;
-		tokens = tokens->next;
-		simple_cmd->num_of_args = count_args(tokens);
-		simple_cmd->args = ft_calloc(simple_cmd->num_of_args, sizeof(char *));
-		i = 0;
-		while (tokens && (tokens->token.type == WORD))
-		{
-			simple_cmd->args[i] = tokens->token.value;
-			i++;
-			tokens = tokens->next;
-		}
-		commands->commands[cmd_i] = simple_cmd;
-		cmd_i++;
 	}
 	return (commands);
 }
@@ -86,6 +155,8 @@ void print_simple_cmd(t_simple_cmd *cmd) {
 		return;
 	}
 
+	ft_printf("Input redirection: %s\n", cmd->in_file.file);
+	ft_printf("Output redirection: %s\n", cmd->out_file.file);
 	ft_printf("Command: %s\n", cmd->command);
 	ft_printf("Args (%d): ", cmd->num_of_args);
 	for (int i = 0; i < cmd->num_of_args; i++) {
@@ -97,16 +168,14 @@ void print_simple_cmd(t_simple_cmd *cmd) {
 void print_commands(t_commands *cmds) {
 	int i = 0;
 
-	if (cmds == NULL) {
+	if (cmds == NULL)
+	{
 		ft_printf("NULL commands structure\n");
 		return;
 	}
-	if (cmds->in_file)
-		ft_printf("Input redirection: %s\n", cmds->in_file);
-	if (cmds->out_file)
-		ft_printf("Output redirection: %s\n", cmds->out_file);
-	while (cmds->commands && cmds->commands[i]) {
-		ft_printf("Command %d:\n", i + 1);
+	while (cmds->commands && cmds->commands[i])
+	{
+		ft_printf("\e[0;32mCommand %d:\e[0m\n", i + 1);
 		print_simple_cmd(cmds->commands[i]);
 		i++;
 	}

4b1b7267e9ff7eec8519e03389ca22c7f0fbff4f
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-05-12 23:49:14 +0300

    Update split.c

--- split.c
+++ split.c
@@ -7,9 +7,14 @@ static int	is_whitespace(char c)
 	return (c == 32 || (c >= 9 && c <= 13));
 }
 
+int	is_quote(char c)
+{
+	return (c == '\"' || c == '\'');
+}
+
 int	is_operator(char c)
 {
-	return (c == '|' || c == '<' || c == '>');
+	return (c == '<<' || c == '>>' || c == '|' || c == '<' || c == '>' );
 }
 
 char	*skip_whitespaces(char *str)

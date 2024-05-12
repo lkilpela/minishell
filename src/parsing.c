256f028cd524c147dd860d7a57cd03f35d882c14
aklein <aklein@student.hive.fi>
2024-05-12 16:42:52 +0300

    edits for tests to work with current token struct

--- src/parsing.c
+++ src/parsing.c
@@ -7,50 +7,59 @@ t_token_list	*init_test()
 	t_token_list	*test;
 	t_token_list	*ptr;
 	test = malloc(sizeof(t_token_list));
+	test->token = malloc(sizeof(t_token));
 	ptr = test;
 
-	test->token.value = "ls";
-	test->token.type = WORD;
+	test->token->value = "ls";
+	test->token->type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = ">";
-	test->token.type = D_GREAT;
+	test->token->value = ">";
+	test->token->type = D_GREAT;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = "myout";
-	test->token.type = WORD;
+	test->token->value = "myout";
+	test->token->type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = ">";
-	test->token.type = GREAT;
+	test->token->value = ">";
+	test->token->type = GREAT;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = "test";
-	test->token.type = WORD;
+	test->token->value = "test";
+	test->token->type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = "-l";
-	test->token.type = WORD;
+	test->token->value = "-l";
+	test->token->type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = "|";
-	test->token.type = PIPE;
+	test->token->value = "|";
+	test->token->type = PIPE;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = "cat";
-	test->token.type = WORD;
+	test->token->value = "cat";
+	test->token->type = WORD;
 	test->next = malloc(sizeof(t_token_list));
 	test = test->next;
+	test->token = malloc(sizeof(t_token));
 
-	test->token.value = "-e";
-	test->token.type = WORD;
+	test->token->value = "-e";
+	test->token->type = WORD;
 	test->next = NULL;
 
 	return (ptr);
@@ -61,18 +70,18 @@ int	count_args(t_token_list *tokens)
 	int	count;
 
 	count = 0;
-	while (tokens && tokens->token.type != PIPE)
+	while (tokens && tokens->token->type != PIPE)
 	{
-		if (tokens->token.type >= LESS && tokens->token.type <=D_GREAT)
+		if (tokens->token->type >= LESS && tokens->token->type <=D_GREAT)
 		{
 			tokens = tokens->next;
-			if (tokens->token.type == WORD)
+			if (tokens->token->type == WORD)
 			{
 				tokens = tokens->next;
 				continue ;
 			}
 		}
-		if (tokens->token.type == WORD)
+		if (tokens->token->type == WORD)
 			count++;
 		tokens = tokens->next;
 	}
@@ -86,26 +95,26 @@ void	heredoc(void)
 
 t_token_list	*get_redir(t_simple_cmd *simple, t_token_list *tokens)
 {
-	if (tokens->token.type == D_LESS)
+	if (tokens->token->type == D_LESS)
 		heredoc();
-	if (tokens->token.type == LESS)
+	if (tokens->token->type == LESS)
 	{
 		tokens = tokens->next;
-		if (tokens->token.type == WORD)
+		if (tokens->token->type == WORD)
 		{
-			simple->in_file.file = tokens->token.value;
+			simple->in_file.file = tokens->token->value;
 			return (tokens->next);
 		}
 	}
-	if (tokens->token.type == GREAT || tokens->token.type == D_GREAT)
+	if (tokens->token->type == GREAT || tokens->token->type == D_GREAT)
 	{
 		simple->out_file.append = 0;
-		if (tokens->token.type == D_GREAT)
+		if (tokens->token->type == D_GREAT)
 			simple->out_file.append = 1;
 		tokens = tokens->next;
-		if (tokens->token.type == WORD)
+		if (tokens->token->type == WORD)
 		{
-			simple->out_file.file = tokens->token.value;
+			simple->out_file.file = tokens->token->value;
 			return (tokens->next);
 		}
 	}
@@ -118,24 +127,24 @@ t_simple_cmd	*simple_cmd(t_token_list **tokens)
 	int				i;
 
 	simple_cmd = ft_calloc(1, sizeof(t_simple_cmd));
-	while ((*tokens) && (*tokens)->token.type != PIPE)
+	while ((*tokens) && (*tokens)->token->type != PIPE)
 	{
-		if ((*tokens)->token.type >= LESS && (*tokens)->token.type <= D_GREAT)
+		if ((*tokens)->token->type >= LESS && (*tokens)->token->type <= D_GREAT)
 		{
 			(*tokens) = get_redir(simple_cmd, *tokens);
 			continue ;
 		}
-		if (simple_cmd->command == NULL && (*tokens)->token.type == WORD)
+		if (simple_cmd->command == NULL && (*tokens)->token->type == WORD)
 		{
-			simple_cmd->command = (*tokens)->token.value;
+			simple_cmd->command = (*tokens)->token->value;
 			(*tokens) = (*tokens)->next;
 			simple_cmd->num_of_args = count_args(*tokens);
 			simple_cmd->args = ft_calloc(simple_cmd->num_of_args, sizeof(char *));
 			i = 0;
 			continue ;
 		}
-		if (simple_cmd->command != NULL && (*tokens)->token.type == WORD)
-			simple_cmd->args[i++] = (*tokens)->token.value;
+		if (simple_cmd->command != NULL && (*tokens)->token->type == WORD)
+			simple_cmd->args[i++] = (*tokens)->token->value;
 		(*tokens) = (*tokens)->next;
 	}
 	return (simple_cmd);

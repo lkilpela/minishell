f57eb3ef717a124435f511e08c3da7a588904113
lumikil <kilpelainenlumi@gmail.com>
2024-05-13 09:45:25 +0300

    Refactor split.c

--- split.c
+++ split.c
@@ -14,7 +14,7 @@ int	is_quote(char c)
 
 int	is_operator(char c)
 {
-	return (c == '<<' || c == '>>' || c == '|' || c == '<' || c == '>' );
+	return (c == '|' || c == '<' || c == '>');
 }
 
 char	*skip_whitespaces(char *str)
@@ -26,14 +26,31 @@ char	*skip_whitespaces(char *str)
 
 char	*skip_word(char *str)
 {
-	while (*str && !is_whitespace(*str) && !is_operator(*str))
+	while (*str && !is_whitespace(*str) && !is_operator(*str) && !is_quote(*str))
 		str++;
 	return (str);
 }
 
 char *skip_op(char *str)
 {
-	while (*str && is_operator(*str))
+	if (is_operator(*str))
+		str++;
+	return (str);
+}
+
+char *skip_quote(char *str)
+{
+	int	in_quote = 1;
+
+	str++;
+	while (in_quote)
+	{
+		if (is_quote(*str))
+			in_quote = 0;
+		else
+			str++;
+	}
+	if (!in_quote)
 		str++;
 	return (str);
 }
@@ -41,10 +58,11 @@ char *skip_op(char *str)
 int count_tokens(char *str)
 {
 	int count = 0;
+
 	while (*str)
 	{
 		str = skip_whitespaces(str);
-		if (*str && !is_operator(*str))
+		if (*str && !is_operator(*str) && !is_quote(*str))
 		{
 			count++;
 			str = skip_word(str);
@@ -54,10 +72,28 @@ int count_tokens(char *str)
 			count++;
 			str = skip_op(str);
 		}
+		else if (*str && is_quote(*str))
+		{
+			count++;
+			str = skip_quote(str);
+		}
 	}
 	return count;
 }
+char *find_token_end(char *str)
+{
+	char	*end;
 
+	end = str;
+	if (is_operator(*str))
+		end++;
+	else if (is_quote(*str))
+		end = skip_quote(str);
+	else
+		while (*end && !is_whitespace(*end) && !is_operator(*end))
+			end++;
+	return (end);
+}
 char	*create_a_token(char *str)
 {
 	char	*a_token;
@@ -65,13 +101,8 @@ char	*create_a_token(char *str)
 	int		len;
 	char	*end;
 
-	end = str;
+	end = find_token_end(str);
 	i = 0;
-	if (is_operator(*str))
-		end++;
-	else
-		while (*end && !is_whitespace(*end) && !is_operator(*end))
-			end++;
 	len = end - str;
 	a_token = malloc(sizeof(char) * (len + 1));
 	if (!a_token)
@@ -83,10 +114,9 @@ char	*create_a_token(char *str)
 	}
 	a_token[len] = '\0';
 	return (a_token);
-
 }
 
-char **split_token(char *str)
+char **split_input(char *str)
 {
 	int	number_token = count_tokens(str);
 	char **tab = malloc(sizeof(char*) * (number_token + 1));
@@ -128,10 +158,11 @@ void	free_arrays(char **arrays)
 // "   echo<|grep >" -> bash: syntax error near unexpected token `|'
 int main()
 {
-	char **tab = split_token("   echo|ls");
-	//int count = count_tokens(str);
-	//printf("count_token: %d\n", count);
-	for (int i = 0; tab[i]; i++)
+	//char **tab = split_token("   echo \"Hello\" |ls");
+	char *str = "echo \"Hello\" |ls";
+	int count = count_tokens(str);
+	printf("count_token: %d\n", count);
+	/*for (int i = 0; tab[i]; i++)
         printf("'%s'\n", tab[i]);
-	free_arrays(tab);
+	free_arrays(tab);*/
 }

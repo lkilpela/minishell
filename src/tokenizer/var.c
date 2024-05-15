b5ca26f8adf5e1f729be4dd49298a05a8673062e
lumikil <kilpelainenlumi@gmail.com>
2024-05-15 11:41:24 +0300

    Refactor var.c for variable expansion

--- src/tokenizer/var.c
+++ src/tokenizer/var.c
@@ -6,13 +6,13 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 14:26:44 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/15 08:40:23 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/15 11:40:52 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <tokenizer.h>
 
-char *find_var_end(char *str)
+/*char *find_var_end(char *str)
 {
 	char	*end;
 
@@ -23,14 +23,39 @@ char *find_var_end(char *str)
 		while (ft_isalnum(*end) || *end == '_')
 			end++;
 	return (end);
-}
+}*/
 
-void	expand_variables(char *str)
+t_var	*expand_variables(t_token *token)
 {
-	char *var_value;
-	const char *var_name = "USER";
+	t_var	*var;
+	char	*env_value;
 
-	var_value = getenv(var_name);
-	printf("var_value: %s\n", var_value);
+	var = malloc(sizeof(t_var));
+	if (!var)
+		return (NULL);
+	if (token->type == VAR)
+	{
+		// store token.value to var.name
+		var->name = ft_strdup(token->value);
+		if (!var->name)
+		{
+			free(var);
+			return (NULL);
+		}
+		env_value = getenv(token->value);
+		if (env_value)
+		{
+			var->value = ft_strdup(env_value);
+			if (!var->value)
+			{
+				free(var->name);
+				free(var);
+				return (NULL);
+			}
+		}
+		else
+			var->value = NULL;
+	}
+	return (var);
 }
 

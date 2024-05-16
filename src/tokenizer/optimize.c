d22e3e1fa34aa491874681a0e90fd8b4b0f264d9
lumikil <kilpelainenlumi@gmail.com>
2024-05-16 15:22:31 +0300

    Refactor is_word and optimize.c to take dollar sign as word

--- src/tokenizer/optimize.c
+++ src/tokenizer/optimize.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 09:18:16 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/16 15:01:37 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/16 15:21:41 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 static char *next_token(char *str)
 {
-	while (*str)
-	{
-		if (is_whitespace(*str))
-			str = skip_whitespaces(str);
-		else if (is_word(*str))
-			str = skip_word(str);
-		else if (is_operator(*str))
-			str = skip_op(str);
-		else if (is_quote(*str))
-			str = skip_quote(str);
-		else if (*str == '$')
-			str = skip_variable(str);
-		else
-			break;
-	}
+	if (is_whitespace(*str))
+		str = skip_whitespaces(str);
+	else if (is_word(*str))
+		str = skip_word(str);
+	else if (is_operator(*str))
+		str = skip_op(str);
+	else if (is_quote(*str))
+		str = skip_quote(str);
+	//else if (*str == '$')
+		//str = skip_variable(str);
 	return (str);
 }
 
@@ -38,8 +33,8 @@ static int 	token_len(char *str)
 	int		len;
 
 	end = next_token(str);
-	printf("end: %s\n", end);
 	len = end - str;
+	printf("end: %s len: %d\n" , end, len);
 	return (len);
 }
 

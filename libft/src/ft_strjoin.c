1fa5c91afc359cc1b9702a70994fff34ada8976f
aklein <aklein@student.hive.fi>
2024-05-19 02:48:25 +0300

    that's embarrassing

--- libft/src/ft_strjoin.c
+++ libft/src/ft_strjoin.c
@@ -6,38 +6,24 @@
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:01:24 by aklein            #+#    #+#             */
-/*   Updated: 2024/02/14 19:47:08 by aklein           ###   ########.fr       */
+/*   Updated: 2024/05/19 02:37:03 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-static char	*ft_strcpy(char *dest, const char *src)
-{
-	char	*dest_start;
-
-	dest_start = dest;
-	while (*src)
-		*dest++ = *src++;
-	*dest = '\0';
-	return (dest_start);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*new_str;
-	char	*new_start;
-	size_t	len;
+	size_t	total_len;
+	size_t	len1;
+	char	*new_string;
 
-	if (!s1 || !s2)
+	len1 = ft_strlen(s1) + 1;
+	total_len = len1 + ft_strlen(s2);
+	new_string = malloc(total_len * sizeof(char));
+	if (!new_string)
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	new_str = malloc(len + 1);
-	if (!new_str)
-		return (NULL);
-	new_start = new_str;
-	ft_strcpy(new_str, s1);
-	new_str += ft_strlen(s1);
-	ft_strcpy(new_str, s2);
-	return (new_start);
+	ft_strlcpy(new_string, s1, len1);
+	ft_strlcat(new_string, s2, total_len);
+	return (new_string);
 }

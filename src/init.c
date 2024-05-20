52782a167b127a5589ab33cb288adf6f7eece579
lumikil <kilpelainenlumi@gmail.com>
2024-05-20 09:20:58 +0300

    u

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 11:04:00 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/19 14:36:43 by aklein           ###   ########.fr       */
+/*   Updated: 2024/05/20 09:20:55 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,3 @@ void init_minishell(int argc, char **argv, char **envp)
 	ms()->envp = envp;
 	ms()->paths = NULL;
 }
-
-/*void init_tokenizer(t_tokenizer *t, char *input)
-{
-	t->input = input;
-	t->pos = 0;
-	init_token(&(t->current_token));	
-	init_token_list(t->tokens);
-}*/
-
-/*void init_var(t_var_array *v)
-{
-	v->size = 0;
-	v->vars = NULL;
-}*/

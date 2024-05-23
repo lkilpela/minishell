3f4c3f7d4b6bb651d8663edbaaee4736c9bdaf5d
lumikil <kilpelainenlumi@gmail.com>
2024-05-23 14:40:36 +0300

    fix norm

--- include/structs.h
+++ include/structs.h
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 10:11:14 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/22 19:34:44 by aklein           ###   ########.fr       */
+/*   Updated: 2024/05/23 14:40:24 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
-typedef struct s_var_list t_var_list;
 /******************************************************************************\
  * MINISHELL
 \******************************************************************************/
@@ -35,7 +34,7 @@ typedef struct s_redir
 	char	*file;
 	int		append;
 	int		fd;
-}	t_redir;
+}				t_redir;
 
 /******************************************************************************\
  * TOKENIZER
@@ -43,12 +42,12 @@ typedef struct s_redir
 typedef enum e_token_type
 {
 	WORD,		// a command (ls) or argument (-l)
-	OP_PIPE,		// pipe output of one cmd into another (|)
-	OP_LESS,		// redirect input from a file (<)
-	OP_GREAT,		// redirect output to a file (>), overwriting file if exists
-	OP_DLESS,		// denote heredoc (<<)
+	OP_PIPE,	// pipe output of one cmd into another (|)
+	OP_LESS,	// redirect input from a file (<)
+	OP_GREAT,	// redirect output to a file (>), overwriting file if exists
+	OP_DLESS,	// denote heredoc (<<)
 	OP_DGREAT,	// redirect output to a file (>>), appending to file if exists
-	VAR, 		// $HOME is treated as a single VAR token´
+	VAR,		// $HOME is treated as a single VAR token´
 	UNKNOWN
 }			t_token_type;
 
@@ -62,41 +61,41 @@ typedef enum e_quote_type
 // single token struct
 typedef struct s_token
 {
-	char		*value; // token string
-	t_token_type type;	// token type
+	char			*value;
+	t_token_type	type;
 }				t_token;
 
 // a token node in linked list
 typedef struct s_token_list
 {
-	t_token				*token;	// current token
-	struct s_token_list	*next;	// pointer to next token
+	t_token				*token;
+	struct s_token_list	*next;
 }				t_token_list;
 
 // variables
-struct s_var_list
+typedef struct s_var_list
 {
-	char				*key;	// name of variable
-	char				*value;	// value of variable
-	struct s_var_list	*next; 
-};
+	char				*key;
+	char				*value;
+	struct s_var_list	*next;
+}				t_var_list;
 
 /******************************************************************************\
  * PARSER
 \******************************************************************************/
 typedef struct s_simple_cmd
 {
-	char	*command;	// value of the command
-	int		num_of_args;	// number of arguments
+	char	*command;
+	int		num_of_args;
 	t_redir	in_file;
 	t_redir	out_file;
-	char 	*heredoc;
+	char	*heredoc;
 	char	**args;
 }			t_simple_cmd;
 
 typedef struct s_commands
 {
-	t_simple_cmd	**simples;		// array of simple cmds
+	t_simple_cmd	**simples;
 	int				num_of_cmds;
 }				t_commands;
 

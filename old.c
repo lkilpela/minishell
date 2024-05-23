f652beac441d400b2b8ab102daa10abd51e8fc6e
lumikil <kilpelainenlumi@gmail.com>
2024-05-23 12:12:37 +0300

    fix norm

--- old.c
+++ old.c
@@ -239,3 +239,55 @@ char 	*handle_quotes(char *str)
 		expanded = ft_strdup(str);
 	return (expanded);
 }
+
+/*char	*check_quotes_and_expand(char *str)
+{
+	t_quote_type	quote_type;
+	char			*s;
+
+	quote_type = NO_QUOTE;
+	s = str;
+	// '$some'"echo "$HOME"" -> final_str: '$some'"echo "User/lumik" $USER"
+	while (*s)
+	{
+		if (quote_type == NO_QUOTE)
+		{
+			if (*s == S_QUOTE)
+				quote_type = SINGLE_QUOTE;
+			else if (*s == D_QUOTE)
+				quote_type = DOUBLE_QUOTE;
+		}
+		else if (quote_type == SINGLE_QUOTE)
+		{
+			if (*s == S_QUOTE)
+				quote_type = NO_QUOTE;
+		}
+		else if (quote_type == DOUBLE_QUOTE)
+		{
+			if (*s == D_QUOTE)
+				quote_type = NO_QUOTE;
+		}
+		if (quote_type != SINGLE_QUOTE && *s == '$')
+		{
+			char *end = skip_variable(s);
+			char *name = ft_strndup(s + 1, end - s - 1);
+			char *value = lookup_var(name);
+			free(name);
+
+			char *prefix = ft_strndup(str, s - str);
+			char *posfix = ft_strdup(end);
+			char *new_str = ft_strjoin(prefix, value);
+			char *final_str = ft_strjoin(new_str, posfix);
+
+			s = final_str + ft_strlen(new_str);
+			
+			free(value);
+			free(prefix);
+			free(posfix);
+			free(new_str);
+			str = final_str;		
+		}	
+		s++;
+	}
+	return (str);
+}*/
--- src/tokenizer/quote_handler.c
+++ src/tokenizer/quote_handler.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:22:23 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/23 12:09:25 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/23 12:12:24 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char	*expand_var(char *s, char **str)
 	char	*posfix;
 	char	*new_str;
 	char	*final_str;
-	
+
 	end = skip_variable(s);
 	name = ft_strndup(s + 1, end - s - 1);
 	value = lookup_var(name);
@@ -55,6 +55,7 @@ char	*expand_var(char *s, char **str)
 	return (s);
 }
 
+	// '$some'"echo "$HOME"" -> final_str: '$some'"echo "User/lumik" $USER"
 char	*check_quotes_and_expand(char *str)
 {
 	t_quote_type	quote_type;
@@ -62,7 +63,6 @@ char	*check_quotes_and_expand(char *str)
 
 	quote_type = NO_QUOTE;
 	s = str;
-	// '$some'"echo "$HOME"" -> final_str: '$some'"echo "User/lumik" $USER"
 	while (*s)
 	{
 		quote_type = update_quote_type(quote_type, *s);
@@ -73,56 +73,3 @@ char	*check_quotes_and_expand(char *str)
 	}
 	return (str);
 }
-
-
-/*char	*check_quotes_and_expand(char *str)
-{
-	t_quote_type	quote_type;
-	char			*s;
-
-	quote_type = NO_QUOTE;
-	s = str;
-	// '$some'"echo "$HOME"" -> final_str: '$some'"echo "User/lumik" $USER"
-	while (*s)
-	{
-		if (quote_type == NO_QUOTE)
-		{
-			if (*s == S_QUOTE)
-				quote_type = SINGLE_QUOTE;
-			else if (*s == D_QUOTE)
-				quote_type = DOUBLE_QUOTE;
-		}
-		else if (quote_type == SINGLE_QUOTE)
-		{
-			if (*s == S_QUOTE)
-				quote_type = NO_QUOTE;
-		}
-		else if (quote_type == DOUBLE_QUOTE)
-		{
-			if (*s == D_QUOTE)
-				quote_type = NO_QUOTE;
-		}
-		if (quote_type != SINGLE_QUOTE && *s == '$')
-		{
-			char *end = skip_variable(s);
-			char *name = ft_strndup(s + 1, end - s - 1);
-			char *value = lookup_var(name);
-			free(name);
-
-			char *prefix = ft_strndup(str, s - str);
-			char *posfix = ft_strdup(end);
-			char *new_str = ft_strjoin(prefix, value);
-			char *final_str = ft_strjoin(new_str, posfix);
-
-			s = final_str + ft_strlen(new_str);
-			
-			free(value);
-			free(prefix);
-			free(posfix);
-			free(new_str);
-			str = final_str;		
-		}	
-		s++;
-	}
-	return (str);
-}*/

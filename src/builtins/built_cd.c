b401bedc811b50042b5f3ffbf0dbd196a120b511
aklein <aklein@student.hive.fi>
2024-05-24 23:25:14 +0300

    added errors to cd

--- src/builtins/built_cd.c
+++ src/builtins/built_cd.c
@@ -6,7 +6,7 @@
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 01:16:39 by aklein            #+#    #+#             */
-/*   Updated: 2024/05/22 19:57:18 by aklein           ###   ########.fr       */
+/*   Updated: 2024/05/24 23:22:07 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,24 @@ void	built_cd(int argc, char **argv)
 
 	if (argc > 1)
 	{
-		//error cd: too many arguments
+		ft_error(WARNING, "minishell: cd: too many arguments", 0);
 		return ;
 	}
 	if (argc == 0)
 	{
 		home = lookup_var("HOME");
 		if (*home == 0)
+		{
+			ft_error(WARNING, "minishell: cd: HOME not set", 0);
 			return ;
-			//error $HOME unset
+		}
 		if (chdir(home) == 0)
 			free(home);
 	}
 	if (argc == 1)
 		if (chdir(argv[0]) != 0)
 		{
-			//error based on errno
+			perror("minishell: cd");
 			return ;
 		}
 }

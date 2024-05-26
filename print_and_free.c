d5620a3ba9851132eca6054d37bfde813377007b
lumikil <kilpelainenlumi@gmail.com>
2024-05-26 14:25:55 +0300

    rename join_and_free to append_heredoc

--- print_and_free.c
+++ print_and_free.c
@@ -125,7 +125,7 @@ void print_simple_cmd(t_simple_cmd *cmd) {
 		return;
 	}
 	if (cmd->heredoc)
-		ft_printf("Heredoc: %s\n", cmd->heredoc);
+		ft_printf("Heredoc: %s", cmd->heredoc);
 	ft_printf("Input redirection: %s\n", cmd->in_file.file);
 	if (cmd->out_file.append)
 		ft_printf("(APPEND) ");

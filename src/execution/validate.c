c492f2d288aee3f34734ee96034061c12c68734a
lumikil <kilpelainenlumi@gmail.com>
2024-05-27 15:38:52 +0300

    implement validate_command function in validate.c

--- src/execution/validate.c
+++ src/execution/validate.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:27:51 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/27 15:28:46 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/27 15:38:11 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void	validate_command(t_commands *c)
 {
-	
+	int i;
+	char *infile;
+
+	i = 0;
+	infile = c->simples[i]->in_file.file;
+	if (i < c->num_of_cmds)
+	{
+		if (ft_strlen())
+	}
 }

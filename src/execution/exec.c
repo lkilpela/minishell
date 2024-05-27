e906670ddbe9636d24b7c8cb76533aeebbee333a
lumikil <kilpelainenlumi@gmail.com>
2024-05-27 09:06:53 +0300

    move functions from exec.c to child.c

--- src/execution/exec.c
+++ src/execution/exec.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 14:38:41 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/27 08:16:20 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/27 09:06:22 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,29 +28,6 @@ void	setup_pipes(t_execution *e)
 		i++;
 	}
 }
-
-int	setup_commands(t_execution *e)
-{
-	int	i;
-	t_simple_cmd *cmd;
-
-	i = 0;
-	cmd = e->cmds->simples[i];
-	while (i < e->cmds->num_of_cmds)
-	{
-		if (dup2(cmd->in_file.fd, STDIN_FILENO) == -1)
-		{
-			close(cmd->in_file.fd);
-			ft_error(FATAL, ERR_DUP2, 1);
-		}
-		if (dup2(e->pipefds[i], STDOUT_FILENO) == -1)
-		{
-			
-		}
-	}
-}
-
-
 
 void execute_cmds(t_commands *cmd, t_execution	*exec)
 {

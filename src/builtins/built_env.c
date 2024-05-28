698acc882bcf5accff405a6c7e060015dbceb0f2
aklein <aklein@student.hive.fi>
2024-05-28 16:19:52 +0300

    built env for export without args, prtints values in double quotes as in bash

--- src/builtins/built_env.c
+++ src/builtins/built_env.c
@@ -6,7 +6,7 @@
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 01:19:10 by aklein            #+#    #+#             */
-/*   Updated: 2024/05/27 00:08:53 by aklein           ###   ########.fr       */
+/*   Updated: 2024/05/28 16:15:32 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,12 @@ void	built_env(int declare)
 		if (envs->key && envs->value)
 		{
 			if (declare)
+			{
 				ft_printf("declare -x ");
-			ft_printf("%s=%s\n", envs->key, envs->value);
+				ft_printf("declare -x %s=\"%s\"\n", envs->key, envs->value);
+			}
+			else
+				ft_printf("%s=%s\n", envs->key, envs->value);
 		}
 		envs = envs->next;
 	}

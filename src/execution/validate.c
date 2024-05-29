9604fe21ed201d73891f5ce80083f55d34f51978
lumikil <kilpelainenlumi@gmail.com>
2024-05-29 10:41:19 +0300

    add file permission validation for input and output files

--- src/execution/validate.c
+++ src/execution/validate.c
@@ -6,11 +6,13 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:27:51 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/28 20:44:35 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/29 10:18:58 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+# define PERMISSION 0644
 
 void	validate_command(t_commands *c)
 {
@@ -28,4 +30,16 @@ void	validate_command(t_commands *c)
 		else if (access(outfile, F_OK) == 0)
 			print_error("minishell: outfile: ", ERR_FILE, NULL, 1);
 	}
+}
+
+void	validate_permission(t_simple_cmd *s)
+{
+	s->in_file.fd = open(s->in_file.file, O_RDONLY);
+	if (s->in_file.fd == -1)
+		ft_error(WARNING, ERR_PERM, 1);
+	s->out_file.fd = open(s->out_file.file,
+			O_CREAT | O_WRONLY | O_TRUNC, PERMISSION);
+	if (s->out_file.fd == -1)
+		ft_error(WARNING, ERR_PERM, 1);
+	
 }

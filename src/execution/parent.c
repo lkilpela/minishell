367fde3130bea2abe5583db179cba4308310ce4a
lumikil <kilpelainenlumi@gmail.com>
2024-05-29 10:04:17 +0300

    refactor parent

--- src/execution/parent.c
+++ src/execution/parent.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:22:07 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/28 21:42:01 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/05/29 10:03:57 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	parent(t_commands *c, int i)
 {
 	while (i < c->num_of_cmds)
 	{
-		c->pid = waitpid(c->pids[i], &c->status, 0);
-		if (c->pid == -1)
+		ms()->pid = waitpid(ms()->pids[i], &ms()->status, 0);
+		if (ms()->pid == -1)
 			ft_error(FATAL, ERR_WAITPID, 1);
-		if (WIFEXITED(c->status))
-			ms()->exit = WEXITSTATUS(c->status);
+		if (WIFEXITED(ms()->status))
+			ms()->exit = WEXITSTATUS(ms()->status);
 		i++;
 	}
 	return (0);

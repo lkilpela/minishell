f9a3355b852f3af10acdc598c437dabb3121f57b
aklein <aklein@student.hive.fi>
2024-05-31 01:13:15 +0300

    command name added to the first element of argv list

--- src/parser/simple_cmd.c
+++ src/parser/simple_cmd.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   simple_cmd.c                                       :+:      :+:    :+:   */
+/*   cmd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 12:03:00 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/05/30 17:56:05 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/31 01:02:44 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,48 +57,49 @@ static int	count_args(t_token_list **tokens)
 	return (count);
 }
 
-static void	parse_command(t_cmd **simp, t_token_list **tokens)
+static void	parse_command(t_cmd *cmd, t_token_list **tokens)
 {
 	if ((*tokens)->type == WORD)
 	{
 		expand_current_el(tokens, tokens);
-		(*simp)->command = (*tokens)->value;
+		cmd->command = (*tokens)->value;
 		(*tokens) = (*tokens)->next;
 	}
 }
 
-static void	parse_args(t_cmd *simp, t_token_list **tokens)
+static void	parse_args(t_cmd *cmd, t_token_list **tokens)
 {
-	simp->num_of_args = count_args(tokens);
-	if (simp->num_of_args > 0)
-		simp->args = ft_safe_calloc(simp->num_of_args + 1, sizeof(char *));
+	cmd->num_of_args = count_args(tokens);
+	cmd->args = ft_safe_calloc(cmd->num_of_args + 2, sizeof(char *));
+	if (cmd->command)
+		cmd->args[0] = cmd->command;
 }
 
 t_cmd	*simple_cmd(t_token_list **tokens)
 {
-	t_cmd	*simple_cmd;
+	t_cmd	*cmd;
 	int				i;
 
 	i = 0;
-	simple_cmd = ft_safe_calloc(1, sizeof(t_cmd));
+	cmd = ft_safe_calloc(1, sizeof(t_cmd));
 	while ((*tokens) && (*tokens)->type != OP_PIPE)
 	{
 		if ((*tokens)->type >= OP_LESS && (*tokens)->type <= OP_DGREAT)
 		{
-			(*tokens) = get_redir(simple_cmd, *tokens);
+			(*tokens) = get_redir(cmd, *tokens);
 			continue ;
 		}
-		if (simple_cmd->command == NULL && (*tokens)->type == WORD)
+		if (cmd->command == NULL && (*tokens)->type == WORD)
 		{
-			parse_command(&simple_cmd, tokens);
-			parse_args(simple_cmd, tokens);
+			parse_command(cmd, tokens);
+			parse_args(cmd, tokens);
 			i = 0;
 			continue ;
 		}
-		if (simple_cmd->command != NULL && (*tokens)->type == WORD)
-			simple_cmd->args[i++] = (*tokens)->value;
+		if (cmd->command != NULL && (*tokens)->type == WORD)
+			cmd->args[i++] = (*tokens)->value;
 		(*tokens) = (*tokens)->next;
 	}
-	return (simple_cmd);
+	return (cmd);
 }
 

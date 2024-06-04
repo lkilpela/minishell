f6546682e419c0a0989265d163de02b2f4bc49db
lumikil <kilpelainenlumi@gmail.com>
2024-06-04 05:37:29 +0300

    fix norm

--- src/error/safe_alloc.c
+++ src/error/safe_alloc.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 05:32:49 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/06/04 05:33:28 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/06/04 05:37:07 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,59 +21,4 @@ void	*ft_safe_calloc(size_t count, size_t size)
 		ft_error(E_CODE_ERRNO + errno);
 	add_to_lal(alloc);
 	return (alloc);
-}
-
-char	*ft_safe_strdup(const char *s1)
-{
-	char	*str;
-
-	str = ft_strdup(s1);
-	if (!str)
-		ft_error(E_CODE_ERRNO + errno);
-	add_to_lal((void *)str);
-	return (str);
-}
-
-char	*ft_safe_strndup(const char *s1, size_t len)
-{
-	char	*str;
-
-	str = ft_strndup(s1, len);
-	if (!str)
-		ft_error(E_CODE_ERRNO + errno);
-	add_to_lal((void *)str);
-	return (str);
-}
-
-char	*ft_safe_strjoin(const char *s1, const char *s2)
-{
-	char	*str;
-
-	str = ft_strjoin(s1, s2);
-	if (!str)
-		ft_error(E_CODE_ERRNO + errno);
-	add_to_lal((void *)str);
-	return (str);
-}
-
-char	*ft_safe_substr(const char *s, unsigned int start, size_t len)
-{
-	char	*str;
-
-	str = ft_substr(s, start, len);
-	if (!str)
-		ft_error(E_CODE_ERRNO + errno);
-	add_to_lal((void *)str);
-	return (str);
-}
-
-char	*ft_safe_itoa(int n)
-{
-	char	*str;
-
-	str = ft_itoa(n);
-	if (!str)
-		ft_error(E_CODE_ERRNO + errno);
-	add_to_lal((void *)str);
-	return (str);
 }

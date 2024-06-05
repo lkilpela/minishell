ec374cb0f88cf2e1d565f768c38ed4e80cd413e5
lumikil <kilpelainenlumi@gmail.com>
2024-06-05 13:02:50 +0300

    fix norm

--- include/structs.h
+++ include/structs.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 10:11:14 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/06/05 03:59:03 by aklein           ###   ########.fr       */
+/*   Updated: 2024/06/05 13:02:44 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,22 @@
 /******************************************************************************\
  * MINISHELL
 \******************************************************************************/
-typedef struct s_var_list t_var_list;
-typedef struct s_token_list t_token_list;
-typedef struct s_cmd t_cmd;
-typedef struct s_builtin t_builtin;
-
 typedef struct s_ms
 {
-	char			**argv;
-	int				argc;
-	char			**envp;
-	char			**paths;	// Array of paths for executable lookup
-	uint8_t			exit;
-	t_var_list		*var_list;
-	t_token_list	*tokens;
-	t_list			*commands;
-	pid_t			*pids;
-	pid_t			pid;
-	int				pipefd[2];
-	int				status;
-	char			*executable;
-	int				cmds_num;
+	char				**argv;
+	int					argc;
+	char				**envp;
+	char				**paths;
+	uint8_t				exit;
+	t_var_list			*var_list;
+	t_token_list		*tokens;
+	t_list				*commands;
+	pid_t				*pids;
+	pid_t				pid;
+	int					pipefd[2];
+	int					status;
+	char				*executable;
+	int					cmds_num;
 	const t_builtin		*builtins;
 }					t_ms;
 
@@ -48,7 +43,7 @@ typedef struct s_builtin
 	char	*name;
 	void	(*func)(t_cmd *);
 	int		special;
-}	t_builtin;
+}				t_builtin;
 
 /******************************************************************************\
  * FILES
@@ -67,8 +62,6 @@ typedef struct s_redir
 	int		fd;
 }				t_redir;
 
-
-
 /******************************************************************************\
  * TOKENIZER
 \******************************************************************************/
@@ -86,9 +79,9 @@ typedef enum e_token_type
 // a token node in linked list
 typedef struct s_token_list
 {
-	char			*value;
-	t_token_type	type;
-	int				expand;
+	char				*value;
+	t_token_type		type;
+	int					expand;
 	struct s_token_list	*next;
 	struct s_token_list	*prev;
 }				t_token_list;
@@ -96,13 +89,13 @@ typedef struct s_token_list
 /******************************************************************************\
  * VARS
 \******************************************************************************/
-struct s_var_list
+typedef struct s_var_list
 {
 	char				*key;
 	char				*value;
-	struct s_var_list	*next; 
-	struct s_var_list	*previous; 
-};
+	struct s_var_list	*next;
+	struct s_var_list	*previous;
+}				t_var_list;
 
 typedef enum e_quote_type
 {

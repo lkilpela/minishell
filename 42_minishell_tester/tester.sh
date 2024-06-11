7367e3645036a861383081e8b38aef532f0b98b1
lumikil <kilpelainenlumi@gmail.com>
2024-06-11 15:09:55 +0300

    update tester path

--- 42_minishell_tester/tester.sh
+++ 42_minishell_tester/tester.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Change if you store the tester in another PATH
-export MINISHELL_PATH=$HOME/minishell
+export MINISHELL_PATH=$HOME/workspace/git/minishell
 export EXECUTABLE=minishell
-RUNDIR=$HOME/minishell/42_minishell_tester
+RUNDIR=$HOME//workspace/git/minishell/42_minishell_tester
 NL=$'\n'
 TAB=$'\t'
 

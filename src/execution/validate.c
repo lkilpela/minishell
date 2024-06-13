4bbecb45a3676ad4f33f3a2988c391e820f97534
lumikil <kilpelainenlumi@gmail.com>
2024-06-13 13:13:12 +0300

    remove printf debugger

--- src/execution/validate.c
+++ src/execution/validate.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:27:51 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/06/13 13:09:17 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/06/13 13:13:06 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ static int	is_directory(t_cmd *cmd)
 
 static void	validate_executable(t_cmd *cmd)
 {
-	printf("validate_executable");
 	cmd->exec_path = find_executable(cmd);
 	if (cmd->exec_path != NULL)
 	{
@@ -54,31 +53,25 @@ static void	validate_executable(t_cmd *cmd)
 
 void	validate_command(t_cmd *cmd)
 {
-	printf("validate_command\n");
 	if (!*cmd->command)
 	{
 		print_error(ERR_MS, cmd->command, ERR_CMD, 0);
 		ms_exit(FATAL, E_CODE_CMD_NFOUND);
 	}
-	printf("1\n");
 	if (get_builtin(cmd).name)
 		return ;
 	if (is_directory(cmd))
 	{
 		if (ft_strchr(cmd->command, '/'))
 		{
-			printf("here1\n");
 			print_error(ERR_MS, cmd->command, ERR_DIR, 0);
 			ms_exit(FATAL, E_CODE_CMD_NEXEC);
 		}
 		else if (access(cmd->command, X_OK) != 0)
 		{
-			printf("here2\n");
 			print_error(ERR_MS, cmd->command, ERR_CMD, 0);
 			ms_exit(FATAL, E_CODE_CMD_NFOUND);
 		}
 	}
-	printf("2\n");
 	validate_executable(cmd);
-	printf("3\n");
 }

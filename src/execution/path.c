432c615014f466735370f5fcd090fc1a23f0465b
lumikil <kilpelainenlumi@gmail.com>
2024-06-13 17:57:50 +0300

    fix norm

--- src/execution/path.c
+++ src/execution/path.c
@@ -6,13 +6,13 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 08:16:21 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/06/13 14:39:20 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/06/13 17:57:33 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static char **ft_safe_split(const char *s, char c)
+static char	**ft_safe_split(const char *s, char c)
 {
 	char	**str_arr;
 	int		i;
@@ -80,13 +80,11 @@ char	*find_executable(t_cmd *cmd)
 		if (access(tmp, F_OK) == 0)
 		{
 			cmd->exec_path = tmp;
-			//ft_printf("found path @ %s\n", cmd->exec_path);
 			return (cmd->exec_path);
 		}
 		ft_free((void **)&tmp);
 		i++;
 	}
-	//ft_printf("couldnt find path for %s\n", command);
 	ft_free((void **)&command);
 	return (NULL);
 }

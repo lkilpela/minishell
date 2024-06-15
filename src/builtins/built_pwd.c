a9e5fc806ebca9d4cc4402cc54f1a0af85228e77
aklein <aklein@student.hive.fi>
2024-06-15 09:45:14 +0300

    getcwd leak fix

--- src/builtins/built_pwd.c
+++ src/builtins/built_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   built_pwd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 01:17:23 by aklein            #+#    #+#             */
-/*   Updated: 2024/06/05 08:41:56 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/06/15 09:42:35 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	built_pwd(t_cmd *cmds)
 	(void)cmds;
 	pwd = getcwd(NULL, 0);
 	ft_printf("%s\n", pwd);
-	ft_free((void **)&pwd);
+	free(pwd);
 	ms()->exit = EXIT_SUCCESS;
 }
